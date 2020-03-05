8555d090e272a86638ef059d6692868a7a7ca13f
Norman Hussey <norm.hussey@gmail.com>
2020-03-05 09:39:01 -0500

    sorted inventory lists alphabetically

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -5,12 +5,24 @@ import InventoryItem from './InventoryItem';
 class Inventory extends Component {
     render() {
         const owner = this.props.owner;
+        const sortedInventory = [...owner.inventory];
+        sortedInventory.sort((a, b) => {
+          const itemA = a.type;
+          const itemB = b.type;
+          let comparison = 0;
+          if (itemA > itemB) {
+            comparison = 1;
+          } else if (itemA < itemB) {
+            comparison = -1;
+          }
+          return comparison;
+        });
         return(
             <div className = "ornateContainer inventory">
             <h3>{owner.name}</h3>
             { owner.maxInventory ? <h4>{owner.inventorySize} / {owner.maxInventory}</h4> : null}
             {
-              owner.inventory.map((item, index) => {
+              sortedInventory.map((item, index) => {
                 if (item.type !== "empty") {
                   return(
                     <InventoryItem item={item} key={index} clickFunction={(item) => { this.props.clickFunction(owner, item) }}/>
